6ba548a2aa9074ac1e4dbf45272fb927735ddf43
Savvas Nicholas <iamyojimbo@users.noreply.github.com>
2018-03-23 14:43:42 -0500

    Update to use environment variables (Untested)

--- outbound.js
+++ outbound.js
@@ -1,19 +1,21 @@
 // Twilio Function deployed at https://www.twilio.com/console/runtime/functions/manage/ZHffa6bf57cf69f0d2ed67b3c29f517fe7
+// 
+// Use:
 // When someone hits the URL specified in the link above, this function will initiate a call
 // to the `fromNumber`, and then, using the TwilioBin, will attempt to connect to the
 // `forwardNumber`.
 
 exports.handler = function(context, event, callback) {
   const sourceCountry = event.sc;
-  const twilioNumber = '###'; // Infuture, TBD by source country
-  const fromNumber = '###';
+  const twilioNumber = context.TWILIO_NUMBER; // In future, TBD by source country
+  const fromNumber = context.FROM_NUMBER;
   const forwardNumber = event.t;
+  const twimlUrl = context.TWIML_URL_BASE + '?';
   
   const queryData = {
     forwardNumber
   };
   const twimlQueryString = encodeQueryData(queryData);
-  const twimlUrl = 'https://handler.twilio.com/twiml/EHeca51f57ae15d1a55a72c776973f8bf4?';
   const url = twimlUrl + twimlQueryString;
   console.log('Twiml Request URL', url);
 
@@ -39,3 +41,5 @@ function encodeQueryData(data) {
    }
    return ret.join('&');
 }
+
+
